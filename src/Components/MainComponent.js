a7782cb1f768415e058d5bac418b50c6f3c399ad
Deanna Shaw <dshaw@reliaslearning.com>
2018-09-27 17:36:35 -0400

    beginning of adding restaurant search

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -10,7 +10,8 @@ class Main extends Component {
             maxEmotion: '',
             food: '',
             emotionString: null,
-            zipcode: '27519'
+            zipcode: '27519',
+            restaurants: null
         };
         this.handleChange = this.handleChange.bind(this);
     }
@@ -26,7 +27,8 @@ class Main extends Component {
     capture = () => {
         // Take Picture and get mood
         const AzureFace_SUBSCRIPTION_KEY = 'b087994442994dbf92cc35088a0dd104';
-        const weatherApiKey = '0a936f2103ce9629dc0c77c09e4a6114'
+        const weatherApiKey = '0a936f2103ce9629dc0c77c09e4a6114';
+        const mapQuest_Api_KEY = '';
 
         this.webcam.getCanvas().toBlob(blob => {
             fetch('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=false&returnFaceLandmarks=false&returnFaceAttributes=emotion',
@@ -40,7 +42,6 @@ class Main extends Component {
                 }).then(response => {
                     return response.json();
                 }).then(data => {
-                    var maxEmotion = '';
                     if (data[0]) {
                         let emotions = data[0].faceAttributes.emotion;
                         var maxEmotionNum = null;
@@ -56,51 +57,57 @@ class Main extends Component {
                     } else {
                         this.setState({ emotionString: "Please try another picture" });
                     }
-                    return maxEmotion;
+                    return this.state.maxEmotion;
                 }
-            ).then(maxEmotion => {
-                // Get the weather
-                let url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + (this.state.zipcode || '27519') + ',us&appid=' + weatherApiKey;
-                fetch(url).then(results => {
-                    return results.json();
-                }).then(data => {
-                    var weather = data.weather[0];
-                    this.setState({weather: weather});
+                ).then(maxEmotion => {
+                    // Get the weather
+                    let url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + (this.state.zipcode || '27519') + ',us&appid=' + weatherApiKey;
+                    fetch(url).then(results => {
+                        return results.json();
+                    }).then(data => {
+                        var weather = data.weather[0];
+                        this.setState({ weather: weather });
+                        // Determine which type of cuisine
+                        this.setState({ food: cuisine[maxEmotion][weather['id']] });
+                    })
 
-                // Determine which type of cuisine
-                    this.setState({food: cuisine[maxEmotion][weather['id']]});
+                    // Find restaurants by cuisine
+                    // fetch(url).then(results => {
+                    //     return results.json();
+                    // }).then(data => {
+                    //     var restaurants = data.weather[0];
+                    //     this.setState({ restaurants: restaurants });
+
+
+                    // })
                 })
-
-                // Find restaurants by cuisine
-            })
         })
-      };
+    };
 
-        render() 
-            {
-                const videoConstraints = {
-                    width: 1280,
-                    height: 720,
-                    facingMode: "user"
-                  };
-            return(
+    render() {
+        const videoConstraints = {
+            width: 1280,
+            height: 720,
+            facingMode: "user"
+        };
+        return (
+            <div>
                 <div>
-                    <div>
-                        <p>
-                            Please enter your zip code:
-                        </p>
-                        <input type = "text" value = {this.state.zipcode} onChange = {this.handleChange}/>
+                    <p>
+                        Please enter your zip code:
+                    </p>
+                    <input type="text" value={this.state.zipcode} onChange={this.handleChange} />
                     <div>
                         <Webcam
-                        audio={false}
-                        height={350}
-                        ref={this.setRef}
-                        screenshotFormat="image/jpeg"
-                        width={350}
-                        videoConstraints={videoConstraints}
+                            audio={false}
+                            height={350}
+                            ref={this.setRef}
+                            screenshotFormat="image/jpeg"
+                            width={350}
+                            videoConstraints={videoConstraints}
                         />
                     </div>
-                    
+
                     <div>
                         <button onClick={this.capture}>
                             Capture photo
@@ -108,15 +115,15 @@ class Main extends Component {
                     </div>
 
                     <div className="weatherContainer">
-                        The weather is: { this.state.weather.description }
+                        The weather is: {this.state.weather.description}
                     </div>
 
                     <div className="weatherContainer">
-                        { this.state.emotionString }
+                        {this.state.emotionString}
                     </div>
-                    
-                    <div className = "weatherContainer">
-                        The food is: { this.state.food }
+
+                    <div className="weatherContainer">
+                        The food is: {this.state.food}
                     </div>
                 </div>
                 <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyAjo2E9Np9S3d6zg60Tz9MaFKoOPCUC1gQ&libraries=places'></script>
